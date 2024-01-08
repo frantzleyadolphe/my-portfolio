d7a862f3ba00400e5e5e7c89081e890f3bdaf881
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2024-01-08 00:27:30 -0500

    ajout de MUI

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,11 @@
+import logo from "../assets/logo.png";
 export default function Home() {
-  return <div className="text-blue-800">HomePage</div>;
+  return (
+    <div className="m-0 p-0 bg-white">
+      <div className="mx-20 h-16  flex flex-row justify-between items-center">
+        <img src={logo} alt="logo" className="h-14" />
+        <h2>React</h2>
+      </div>
+    </div>
+  );
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Pages";
 
 const RouteNavigation = () => {
