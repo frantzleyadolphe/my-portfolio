35350204596f673a5ee95d8633ee30835633b444
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2024-01-07 23:07:07 -0500

    some pages created

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "../Pages";
+
+const RouteNavigation = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+export default RouteNavigation;
