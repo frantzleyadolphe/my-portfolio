25fd4a255df9b3b5730658353739817550f3c6b4
Frantzley Adolphe <dolphdjyangelo@gmail.com>
2024-01-10 18:56:46 -0500

    SpeedInsights vercel added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import RouteNavigation from "./routes";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 function App() {
   return (
     <div className="App">
+      <SpeedInsights />
       <RouteNavigation />
     </div>
   );
